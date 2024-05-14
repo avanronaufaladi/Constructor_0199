e877aea9e24be38f52f438fd0215a397fa37e22f
avanronaufaladi <avaroadi@gmail.com>
2024-05-14 13:04:59 +0700

    memasukkan class mahasiswa

--- static_function/static_function.cpp
+++ static_function/static_function.cpp
@@ -2,4 +2,24 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim;/*Definisi function*/ }
+	static int getNim() {
+		return nim; /*Definisi function*/
+	}
+
+	mahasiswa(string pnama):nama (pnama) { 
+		setID(); 
+	}
+
+};
 
